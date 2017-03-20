e7a5e12701715285744f8f1ae9fd5344ee10fbdc
Elizabeth Marais <Elizabeth.Marais@GMAIL.COM>
2017-03-20 21:04:36 +0200

    Add files via upload

--- Cr3Ch1_Networkprog_BS.py
+++ Cr3Ch1_Networkprog_BS.py
@@ -1,14 +1,7 @@
 #Coursera Python Course 3, Chapter 1: Networked programs
 #Using BeautifulSoup for easier string accessing
 
-
 import urllib
-from BeautifulSoup import *
-url = raw_input('Enter file to open: ')
-html = urllib.urlopen(url).read()
-soup = BeautifulSoup(html)
-
-
 print " "
 print "Accessing and printing whathour.py file from the PY4inf.com web page."
 fhand = urllib.urlopen('http://www.py4inf.com/code/whathour.py')
@@ -17,7 +10,7 @@ for line in fhand:
 
 
 print " "
-print  "Print all lines from 'http://www.python.org': "
+print  "Print all url lines from 'http://www.python.org': "
 from BeautifulSoup import BeautifulSoup
 import urllib2
 import re
